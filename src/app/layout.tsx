ffaa63ce033742cf6ac6321f9a6502533e669390
fadilah arifki <67142128+fadilaharifki@users.noreply.github.com>
2025-10-07 12:28:37 +0700

    add meta pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -61,6 +61,42 @@ export default function RootLayout({
         <BottomBar></BottomBar>
         <WhatsAppIcon />
         <Toaster />
+
+        <script>
+          !(function (f, b, e, v, n, t, s) {
+            if (f.fbq) return;
+            n = f.fbq = function () {
+              n.callMethod
+                ? n.callMethod.apply(n, arguments)
+                : n.queue.push(arguments);
+            };
+            if (!f._fbq) f._fbq = n;
+            n.push = n;
+            n.loaded = !0;
+            n.version = '2.0';
+            n.queue = [];
+            t = b.createElement(e);
+            t.async = !0;
+            t.src = v;
+            s = b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t, s);
+          })(
+            window,
+            document,
+            'script',
+            'https://connect.facebook.net/en_US/fbevents.js',
+          );
+          fbq('init', '696812539459141');
+          fbq('track', 'PageView');
+        </script>
+      
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style="display: none"
+            src="https://www.facebook.com/tr?id=696812539459141&ev=PageView&noscript=1" />
+        </noscript>
       </body>
     </html>
   );
